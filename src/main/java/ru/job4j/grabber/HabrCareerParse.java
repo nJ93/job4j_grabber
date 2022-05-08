b411bfc7b4502dc3ffde24e38d583e98bb9692b5
nJ93 <vit-chembaev@yandex.ru>
2022-05-08 20:51:13 +0300

    Парсинг HTML страницы. [#260358]. Attr value instead text

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -24,7 +24,7 @@ public class HabrCareerParse {
       Element linkElement = titleElement.child(0);
       Element dateInnerElement = dateElement.child(0);
       String vacancyName = titleElement.text();
-      String dateStr = dateInnerElement.text();
+      String dateStr = dateInnerElement.attr("datetime");
       String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
       System.out.printf("%s | %s %s%n", dateStr, vacancyName, link);
     });
